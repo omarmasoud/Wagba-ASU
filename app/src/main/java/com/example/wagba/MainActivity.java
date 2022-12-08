4fd65dcea697ac79d30ccbca3d3ad4ba9a8199e2
omarmasoud <omark897@gmail.com>
2022-12-09 01:46:45 +0200

    Minimal design additions

--- app/src/main/java/com/example/wagba/MainActivity.java
+++ app/src/main/java/com/example/wagba/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.wagba;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
+
+import com.example.wagba.AuthenticationScreens.LoginScreen;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Timer timer=new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Fragment loginScreen=new LoginScreen();
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentcontainer, loginScreen);
+                fragmentTransaction.commit();
+            }
+        }, 1500);
     }
 }
